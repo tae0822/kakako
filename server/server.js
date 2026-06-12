8d9a3774e14be5f389923b685bd9472e3c7c49a5
MisoWan <jopd4@naver.com>
2026-06-12 16:42:29 -0400

    fix cors

--- server/server.js
+++ server/server.js
@@ -41,12 +41,17 @@ cors: {
   }
 })
 
-app.use(cors({
-  origin: ["*"],
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  credentials: true,
-  allowedHeaders: ["Content-Type", "Authorization"]
-}));
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*"); // 모든 출처 허용
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  
+  // OPTIONS 요청(Preflight)에 대해 즉시 200 OK 응답
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+  }
+  next();
+});
 
 
 app.use(express.json())
